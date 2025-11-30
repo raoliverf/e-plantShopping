79f2ad078db97d5fd9e6985266e4340d9f7eaa73
raoliverf <raisafigueiroli@hotmail.com>
2025-11-30 01:01:45 -0500

    Madicionando produtos

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+     dispatch(addItem(product));
+
+     setAddedToCart((prevState) => ({ 
+      ...prevState, 
+      [product.name]: true, 
+     }));
+    };
 
     const plantsArray = [
         {
@@ -274,9 +288,32 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
-                </div>
+                    {plantsArray.map((category, index) => ( 
+  <div key={index}> 
+    <h1>
+      <div>{category.category}</div> 
+    </h1>
+    <div className="product-list"> 
+      {category.plants.map((plant, plantIndex) => ( 
+        <div className="product-card" key={plantIndex}> 
+          <img 
+            className="product-image" 
+            src={plant.image}
+            alt={plant.name} 
+          />
+          <div className="product-title">{plant.name}</div> 
+          <div className="product-description">{plant.description}</div> 
+          <div className="product-cost">${plant.cost}</div> 
+          <button
+            className="product-button"
+            onClick={() => handleAddToCart(plant)} >{addedToCart[plant.name] ? "Added" : "Add to Cart"}
+            </button>
+        </div>
+      ))}
+    </div>
+  </div>
+))}
+            </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
             )}
